3f69ba445533d3c5c10ed7d43b6449119bcd0077
Gursimran <Gursimran1@MacBook-Pro.local>
2025-01-03 11:35:28 +0000

    main changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,29 @@
 #!/bin/sh
 
-echo 'Giving unlimited stack size'
-ulimit -s unlimited
-
+# Run migrations
 echo 'Running migrations...'
 python manage.py migrate
 
-echo 'Adding crontab...'
+printenv > /etc/environment
+
+
+# Add crontab
+echo "Adding crontab..."
 python manage.py crontab add
 
-echo 'Starting crond...'
-crond -b
+# Make sure crontab is owned by root and has correct permissions
+touch /var/spool/cron/crontabs/root
+chmod 600 /var/spool/cron/crontabs/root
+chown root:crontab /var/spool/cron/crontabs/root
 
+# Start cron service in the background
+echo "Starting crond..."
+cron
+
+# Verify crontab was added
+echo "Verifying crontab..."
+crontab -l
+python manage.py crontab show
+
+# Execute the command passed to the container
 exec "$@"
